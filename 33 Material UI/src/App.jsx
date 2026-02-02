e449e4118864a44241af70cb6d4e8a4ae96e8901
sachindataninja123 <sachinkumarkushwaha991071@gmail.com>
2026-02-02 20:17:45 +0530

    Material UI

---
+++ 33 Material UI/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./App.css"
+import Buttons from './components/Buttons'
+import UploadFilesButton from './components/UploadFilesButton'
+
+const App = () => {
+  return (
+    <div>
+      <Buttons />
+      <UploadFilesButton />
+    </div>
+  )
+}
+
+export default App
